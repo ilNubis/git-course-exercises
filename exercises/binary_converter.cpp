298ca784831d4388d7a94c46a844589e265f4498
Valerio <valerio.panebianco.04@gmail.com>
2025-11-07 17:44:57 +0100

    feat(func): implemented to_binary

--- exercises/binary_converter.cpp
+++ exercises/binary_converter.cpp
@@ -5,3 +5,47 @@
   Insert first number: 8
   The binary number is: 1000
 */
+
+#include <iostream>
+#include <string>
+
+
+std::string to_binary(int num) {
+  size_t size = sizeof(num) * 8;
+  int mask = (1 << size-1); //2147483648;
+  auto buffer = std::string(size, '\0');
+
+  int started = 0;
+  int skips = 0;
+  for (int i = 0; (i + skips) < size; i++){
+    if (started == 0) {
+        started = (num & mask);
+        
+        if (started == 0) {
+            skips++;
+            i--;
+        }
+    }
+    
+    if (started){
+        switch (num & mask){
+            case 0:
+                buffer[i] = '0';
+                break;
+            default:
+                buffer[i] = '1';
+                break;
+        }
+        
+    }
+    num = num << 1;
+  }
+
+  return buffer;
+}
+
+
+int main(void){
+  std::cout << to_binary(8) << std::endl;
+  return 0;
+}
